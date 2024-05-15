9fde9036e4120d0fb91b0769e4c42b997432c5c6
Nasreen khan <160172100+Nasreenkh@users.noreply.github.com>
2024-05-16 00:55:02 +0500

    This is 45 Question Assignment Q-20.
    This is 45 Question Assignment Q-20.

---
+++ app.js
@@ -0,0 +1,7 @@
+// Array of programming languages
+var programmingLanguages = ["JavaScript", "Python", "Java", "C++", "TypeScript", "Ruby", "Go", "Swift", "Rust"];
+// Print the list of programming languages
+console.log("List of programming languages:");
+programmingLanguages.forEach(function (language) {
+    console.log(language);
+});
---
+++ app.ts
@@ -0,0 +1,15 @@
+/*question no 20. Think of something you could store in a array. For example, you could make a list of mountains,
+rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list
+containing these items. */
+
+
+
+
+// Array of programming languages
+let programmingLanguages: string[] = ["JavaScript", "Python", "Java", "C++", "TypeScript", "Ruby", "Go", "Swift", "Rust"];
+
+// Print the list of programming languages
+console.log("List of programming languages:");
+programmingLanguages.forEach(language => {
+    console.log(language);
+});
